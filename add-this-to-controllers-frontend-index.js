6d4449942e62c93f50ab51ce5c72065a1587ec23
A. M. Douglas <amd@xmli5.io>
2016-02-08 13:49:09 +0000

    Update add-this-to-controllers-frontend-index.js

--- add-this-to-controllers-frontend-index.js
+++ add-this-to-controllers-frontend-index.js
@@ -1,1 +1,57 @@
+// var ...
+elasticlunr = require('elasticlunr'),
 
+// frontendControllers = {
+// ...
+procSearch: function procSearch(req, res){
+
+        var dataquery = req.body.query;
+
+        function dataset(apidata){
+            var docs=[];
+            for(var i=apidata.posts.length,j=0;j<i;j++){
+              docs.push({
+                "id":apidata.posts[j].id,
+                "title":apidata.posts[j].title,
+                "body":apidata.posts[j].markdown
+              });
+            }
+            return search(docs);
+        }
+        
+        function search(docs){
+          var idx=elasticlunr();
+              idx.addField('title');
+              idx.addField('body');
+              idx.setRef('id');
+              idx.saveDocument(false);
+
+          for(var i=docs.length,j=0;j<i;j++){
+            idx.addDoc(docs[j]);
+          }
+          
+          function matches(dataquery){
+            return idx.search(dataquery,{
+              fields:{
+                title:{
+                  boost:2,
+                  bool:"AND"
+                },
+                body:{boost:1}
+              },
+              bool:"OR",
+              expand:true
+            });
+          }
+          return matches(dataquery);
+        }
+        
+        api.posts.browse({status:'published',include:'title,markdown'})
+        .then(function (result){
+            return res.status(200).send(dataset(result))})
+        .catch(function (error){
+            return res.status(200).send("Error: ",error);
+        });
+        
+    },
+    // next controller
